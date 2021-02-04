55f2a0204832b301c5f046bbf4ab0674259b5464
Anthony-BouKhalil <51209624+Anthony-BouKhalil@users.noreply.github.com>
2021-02-04 12:03:03 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,6 +34,7 @@ def message(payload):
     channel_id = event.get('channel')
     user_id = event.get('user')
     
+    
     text2 = event.get('text')
     if BOT_ID !=user_id:
         client.chat_postMessage(channel=channel_id, text=text2)
